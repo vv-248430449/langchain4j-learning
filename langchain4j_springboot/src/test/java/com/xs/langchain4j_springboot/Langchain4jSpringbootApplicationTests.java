ad96bc8d51f7a55e0414e532b55b4fdb82cb7456
vv-248430449 <248430449@qq.com>
2026-08-08 18:10:54 +0800

    test:测试随机字符串：int dimension = embeddingModel.embed("probe-text123").content().dimension(); 长度

--- langchain4j_springboot/src/test/java/com/xs/langchain4j_springboot/Langchain4jSpringbootApplicationTests.java
+++ langchain4j_springboot/src/test/java/com/xs/langchain4j_springboot/Langchain4jSpringbootApplicationTests.java
@@ -1,13 +1,19 @@
 package com.xs.langchain4j_springboot;
 
+import dev.langchain4j.model.embedding.EmbeddingModel;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Langchain4jSpringbootApplicationTests {
 
+    @Autowired
+    private EmbeddingModel embeddingModel;
     @Test
     void contextLoads() {
+        int dimension = embeddingModel.embed("probe-text123").content().dimension();
+        System.out.println("dimension = " + dimension);
     }
 
 }
